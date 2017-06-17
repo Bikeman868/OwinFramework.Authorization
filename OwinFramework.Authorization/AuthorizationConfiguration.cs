78dfe1d5954dd47a948951f7f44dc66e44086995
Martin Halliday <martin.bikeman@gmail.com>
2017-06-17 02:15:24 -0700

    Initialized directory with solution, project and NuGet files

---
+++ OwinFramework.Authorization/AuthorizationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OwinFramework.Authorization
+{
+    [Serializable]
+    internal class AuthorizationConfiguration
+    {
+        public string DocumentationRootUrl { get; set; }
+
+        public AuthorizationConfiguration()
+        {
+            DocumentationRootUrl = "/owin/authorization/config";
+        }
+    }
+}
---
+++ OwinFramework.Authorization/AuthorizationMiddleware.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using OwinFramework.Interfaces.Builder;
+using OwinFramework.Interfaces.Routing;
+using OwinFramework.InterfacesV1.Capability;
+using OwinFramework.InterfacesV1.Middleware;
+using OwinFramework.InterfacesV1.Upstream;
+using OwinFramework.MiddlewareHelpers.SelfDocumenting;
+
+namespace OwinFramework.Authorization
+{
+    public class AuthorizationMiddleware:
+        IMiddleware<IAuthorization>,
+        IUpstreamCommunicator<IUpstreamAuthorization>,
+        IConfigurable,
+        ISelfDocumenting
+    {
+        private readonly IList<IDependency> _dependencies = new List<IDependency>();
+        IList<IDependency> IMiddleware.Dependencies { get { return _dependencies; } }
+        string IMiddleware.Name { get; set; }
+
+        Task IRoutingProcessor.RouteRequest(IOwinContext context, Func<Task> next)
+        {
+            return next();
+        }
+
+        Task IMiddleware.Invoke(IOwinContext context, Func<Task> next)
+        {
+            if (!string.IsNullOrEmpty(_configuration.DocumentationRootUrl) &&
+                context.Request.Path.Value.Equals(_configuration.DocumentationRootUrl, StringComparison.OrdinalIgnoreCase))
+            {
+                return DocumentConfiguration(context);
+            }
+            
+            return next();
+        }
+
+        #region IConfigurable
+
+        private IDisposable _configurationRegistration;
+        private AuthorizationConfiguration _configuration = new AuthorizationConfiguration();
+
+        void IConfigurable.Configure(IConfiguration configuration, string path)
+        {
+            _configurationRegistration = configuration.Register(
+                path,
+                cfg =>
+                {
+                    _configuration = cfg;
+                },
+                new AuthorizationConfiguration());
+        }
+
+        #endregion
+
+        #region ISelfDocumenting
+
+        private Task DocumentConfiguration(IOwinContext context)
+        {
+            var document = GetScriptResource("configuration.html");
+            document = document.Replace("{documentationRootUrl}", _configuration.DocumentationRootUrl);
+
+            var defaultConfiguration = new AuthorizationConfiguration();
+            document = document.Replace("{documentationRootUrl.default}", defaultConfiguration.DocumentationRootUrl);
+
+            context.Response.ContentType = "text/html";
+            return context.Response.WriteAsync(document);
+        }
+        
+        IList<IEndpointDocumentation> ISelfDocumenting.Endpoints
+        {
+            get 
+            {
+                var documentation = new List<IEndpointDocumentation>();
+                if (!string.IsNullOrEmpty(_configuration.DocumentationRootUrl))
+                {
+                    documentation.Add(
+                        new EndpointDocumentation
+                        {
+                            RelativePath = _configuration.DocumentationRootUrl,
+                            Description = "Documentation for the configuratrion of this middleware",
+                            Attributes = new List<IEndpointAttributeDocumentation>
+                                {
+                                    new EndpointAttributeDocumentation
+                                    {
+                                        Type = "Method",
+                                        Name = "GET",
+                                        Description = "Returns documentation on the configuration of the Authorization middleware."
+                                    }
+                                }
+                        });
+                }
+                return documentation;
+            }
+        }
+
+        Uri ISelfDocumenting.GetDocumentation(DocumentationTypes documentationType)
+        {
+            switch (documentationType)
+            {
+                case DocumentationTypes.Configuration:
+                    return new Uri(_configuration.DocumentationRootUrl, UriKind.Relative);
+                case DocumentationTypes.Overview:
+                    return new Uri("https://github.com/Bikeman868/OwinFramework.Authorization", UriKind.Absolute);
+                case DocumentationTypes.SourceCode:
+                    return new Uri("https://github.com/Bikeman868/OwinFramework.Authorization/tree/master/OwinFramework.Authorization", UriKind.Absolute);
+            }
+            return null;
+        }
+
+        string ISelfDocumenting.LongDescription
+        {
+            get { return "Provides a mechanism for defining user groups, roles and permissionn"; }
+        }
+
+        string ISelfDocumenting.ShortDescription
+        {
+            get { return "Provides a mechanism for defining user groups, roles and permissionn"; }
+        }
+
+        #endregion
+
+        #region Embedded resources
+
+        private string GetScriptResource(string filename)
+        {
+            var scriptResourceName = Assembly.GetExecutingAssembly()
+                .GetManifestResourceNames()
+                .FirstOrDefault(n => n.Contains(filename));
+            if (scriptResourceName == null)
+                throw new Exception("Failed to find embedded resource " + filename);
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(scriptResourceName))
+            {
+                if (stream == null)
+                    throw new Exception("Failed to open embedded resource " + scriptResourceName);
+
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
